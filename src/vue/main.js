23d3eb91100e542d67dcd940e20fba020871ac33
Jimmie Max <jimmiemaxgo@gmail.com>
2021-04-25 16:23:44 +0800

    ...

--- src/vue/main.js
+++ src/vue/main.js
@@ -1,3 +1,12 @@
+/*
+ * @Desc: 页面、组件说明
+ * @Date: 2019-06-03 14:34:36
+ * @query: {string} p1  内容ID
+ * @props: {string} p1  数据源
+ * @event: {string} p1  des
+ */
+import Vue from 'vue';
+
 new Vue({
-    template: '<div>{{ hi }}</div>'
-  })
+  template: '<div>{{ hi }}</div>'
+})
